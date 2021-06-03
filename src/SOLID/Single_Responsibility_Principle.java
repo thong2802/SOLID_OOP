56b963695e917ff068bd64fc412d920f7974ae99
MACBOOK <macbook@MACBOOKs-MacBook-Pro-2.local>
2021-06-03 19:26:07 +0700

    update

---
+++ src/SOLID/Single_Responsibility_Principle.java
@@ -0,0 +1,28 @@
+package SOLID;
+public class Single_Responsibility_Principle {
+}
+
+// A class should have only a single Responsibility
+// Ý Tưởng của nguyên lí này giúp giảm đi sự phức tạp của class : một class chỉ nên phục vụ một nhiệm vụ duy nhất .
+// Example :
+// + Thay vì một class ôm nguyên một  các xủ lí như này :
+public class Blogger {
+    public void WriterBlog(){
+        writer();
+    }
+    public void ReadBlog(){
+        read();
+    }
+}
+// Thì chúng ta áp dụng nguyên tắc S : Single responsibility principle như sau :
+// Tach class thực hiện một nhiệm vụ duy nhất.
+ public class writer {
+    public void Writerblog(){
+        writer();
+    }
+}
+public class read {
+    public void readBlog(){
+        read();
+    }
+}
